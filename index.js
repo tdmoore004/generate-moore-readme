8f3012547b9ee4b9625868030d2f10c37b8a3e33
Tanner Moore <tdmoore004@gmail.com>
2020-09-21 16:19:30 -0600

    Successfully added text to README file

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const inquirer = require ("inquirer");
+const fs = require ("fs");
+const generateMarkdown = require ("./utils/generateMarkdown")
 
 // array of questions for user
 const questions = [
@@ -7,11 +9,20 @@ const questions = [
 
 // function to write README file
 function writeToFile(fileName, data) {
+    fs.writeFile("README.md", generateMarkdown(), function(err) {
+    
+        if (err) {
+          return console.log(err);
+        }
+      
+        console.log("README Generated Successfully!");
+      
+      });
 }
 
 // function to initialize program
 function init() {
-
+    writeToFile();
 }
 
 // function call to initialize program
