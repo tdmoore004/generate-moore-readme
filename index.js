a51f6db60e681ad8164db28a5d82ebf6d1922027
Tanner Moore <tdmoore004@gmail.com>
2020-09-23 21:34:03 -0600

    Added additional sections to the README and the questions for the content of those sections.

--- index.js
+++ index.js
@@ -29,6 +29,52 @@ function init() {
             name: "title",
             message: "What is the title of your project?"
           },
+        {
+            type: "input",
+            name: "description",
+            message: "Describe your application?"
+          },
+        {
+            type: "input",
+            name: "installation",
+            message: "Describe how to install your application?"
+          },
+        {
+            type: "input",
+            name: "usage",
+            message: "Describe the usage cases for your application?"
+          },
+        {
+            type: "input",
+            name: "contribution",
+            message: "What are the guidelines for contributing to your application?"
+          },
+        {
+            type: "input",
+            name: "tests",
+            message: "Are there any tests in your code and how do you run them?"
+          },
+        {
+            type: "list",
+            message: "Choose a license for your project?",
+            name: "license",
+            choices: [
+              "MIT", 
+              "GNU GPLv3", 
+              "Apache 2.0", 
+              "ISC"
+            ]
+          },
+        {
+            type: "input",
+            name: "github",
+            message: "What is your GitHub username for questions?"
+          },
+        {
+            type: "input",
+            name: "email",
+            message: "What is your email for contact?"
+          },
     ])
     .then(function(data) {
         console.log(data.title);
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,45 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `[![License: ${data.license}](https://img.shields.io/badge/License-IPL%201.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)
+# ${data.title}
 
+## Description
+
+${data.description}
+
+## Table of Contents
+
+[Installation](##installation)
+[Usage](##usage)
+[License](##license)
+[Contributing](##contributing)
+[Tests](##tests)
+[Questions](##questions)
+
+## Installation
+
+${data.installation}
+
+## Usage
+
+${data.usage}
+
+## License
+
+[${data.license}](https://choosealicense.com/licenses/${data.license}/)
+
+## Contributing
+
+${data.contribution}
+
+## Tests
+
+${data.tests}
+
+## Questions
+
+GitHub: [${data.github}](https://github.com/${data.github})
+Email: [${data.email}](mailto:${data.email})
 `;
   
 }
